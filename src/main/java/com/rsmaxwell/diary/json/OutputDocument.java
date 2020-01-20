717570bd18039a809ecd3b8733120f93ec5beaef
Richard Maxwell <richard@rsmaxwell.co.uk>
2020-01-20 12:34:26 +0000

    initial version

---
+++ src/main/java/com/rsmaxwell/diary/json/OutputDocument.java
@@ -0,0 +1,11 @@
+package com.rsmaxwell.diary.json;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.rsmaxwell.diary.json.OutputDay;
+
+public class OutputDocument {
+
+	public List<OutputDay> days = new ArrayList<OutputDay>();
+}
