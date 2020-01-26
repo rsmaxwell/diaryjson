76182096f73436f0e49c5bade59e22d3287467cf
Richard Maxwell <richard@rsmaxwell.co.uk>
2020-01-26 12:49:02 +0000

    extract text form pages into day files

--- src/main/java/com/rsmaxwell/diaryjson/OutputDay.java
+++ src/main/java/com/rsmaxwell/diaryjson/OutputDay.java
@@ -3,12 +3,12 @@ package com.rsmaxwell.diaryjson;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
-public class OutputDay {
+public class OutputDay implements Comparable {
 
 	public int year;
 	public int month;
 	public int day;
-	public String tag;
+	public String page;
 	public String line;
 
 	@JsonInclude(Include.NON_EMPTY)
@@ -16,5 +16,20 @@ public class OutputDay {
 
 	@JsonInclude(Include.NON_EMPTY)
 	public String notes;
+
+	@Override
+	public int compareTo(Object o) {
+		OutputDay other = (OutputDay) o;
+		if (year != other.year) {
+			return year - other.year;
+		}
+		if (month != other.month) {
+			return month - other.month;
+		}
+		if (day != other.day) {
+			return day - other.day;
+		}
+		return page.compareTo(other.page);
+	}
 
 }
